f956e0554060cff0ceccd00a847760bb4a6675f5
Filipi Vianna <filipi@pucrs.br>
2020-03-08 00:27:14 -0300

    Simple changes to make easy to work on multiple platforms

--- altimetro/altimetro.ino
+++ altimetro/altimetro.ino
@@ -7,13 +7,20 @@
 //               LED
 //               Servo 9G(modificado para atuar como motor DC)
 
+/** Change log
+ * 
+ * Reindentation to save horizontal space
+ * Move getPressure() function to before setup, if using a prototype for
+ * this function the function can go back to the end or to another file...
+ */
+
+
 #include <SFE_BMP180.h>
 #include <Wire.h>
 
 SFE_BMP180 altimetro; // Objeto sensor
 
 //Definiушes Globais
-
 #define PQD_SERVO 9          //Endereуo do motor de acionamento da recuperaусo
 #define LED_1 7              //Endereуo do LED 
 #define intervalo 50         //Intervalo de coleta de dados do sensor BMP 180
@@ -22,10 +29,7 @@ SFE_BMP180 altimetro; // Objeto sensor
 #define t_separa 1000        //Tempo entre amostras de informaусo
 #define t_info 300           //Intervalo da piscada de informaусo
 
-//Variрveis
-
-
-
+//Variaveis
 boolean armado = false;       //Situação do paraquedas
 boolean pisca = false;        //Comutador do pisca-pisca
 double nivel;                 //Pressсo de referЖncia para altitude = 0
@@ -36,63 +40,83 @@ double mem_apg=0;             //Variável de trabalho do Apogeu
 int disp_alt[4];              //Vetor de dados para os algarismos de altitude
 int cont;                     //Contador (aux)
 
-void setup()
-{
+
+double getPressure(){
+  char status;
+  double T,P,p0,a;
+
+  status = altimetro.startTemperature();      //Inicializa o termometro, retorna o tempo de espera em ms ou 0 para erro
+  if (status != 0)
+    {
+      delay(status);                            //Espera atж a finalizar a leitura
+      status = altimetro.getTemperature(T);     //LЖ a temperatura e armazena em T.
+    
+      if (status != 0)
+	{
+	  status = altimetro.startPressure(3);    //inicializa o barometro.
+	  if (status != 0)
+	    {
+	      delay(status);
+	      status = altimetro.getPressure(P,T);  //LЖ a pressсo e armazena em P, com T como temperatura de referЖncia
+
+	      if (status != 0)
+		{
+		  return(P);            
+		}
+	    }
+	}
+    }
+}
+
+void setup(){
   pinMode(PQD_SERVO, OUTPUT);
   pinMode(LED_1, OUTPUT);
   Serial.begin(9600);
   
-    if(altimetro.begin())
-      {
-        nivel = getPressure();                    //inicializa o sensor
-        armado  = true;                           //paraquedas armado
-      }
-    else
-      {
-        while(1)
-            digitalWrite(LED_1,HIGH);             //Mantжm o LED ligado, sinalizando falha.
-      }
+  if(altimetro.begin()) {
+    nivel = getPressure();                    //inicializa o sensor
+    armado  = true;                           //paraquedas armado
+  }
+  else {
+    while(1)
+      digitalWrite(LED_1,HIGH);             //Mantжm o LED ligado, sinalizando falha.
+  }
 }
 
-void loop() 
-{
+void loop() {
   
-  while(armado==true)
-    {
+  while(armado==true){
           
-          pres = getPressure(); 
-          alti = altimetro.altitude(pres,nivel);    //Coleta pressсo e determina altitude em relaусo ao nьvel capturado ao iniciar o programa
-          Serial.println(alti);
+    pres = getPressure(); 
+    alti = altimetro.altitude(pres,nivel);    //Coleta pressсo e determina altitude em relaусo ao nьvel capturado ao iniciar o programa
+    Serial.println(alti);
           
-          digitalWrite(LED_1,pisca);                //Pisca o LED a cada leitura, informando sistema sadio.
-          pisca=!pisca;
+    digitalWrite(LED_1,pisca);                //Pisca o LED a cada leitura, informando sistema sadio.
+    pisca=!pisca;
   
-          if (alti>apogeu)
-              apogeu=alti;                          //Armazena o apogeu
-          else
-            {
-               if (apogeu-alti>abertura)            //Aciona o pqd em uma distРncia determinada abaixo do apogeu.
-                 {
-                    digitalWrite(PQD_SERVO,HIGH);
-                    delay(t_acionamento);           //Aciona o Servo do PQD
-                    digitalWrite(PQD_SERVO,LOW);
-                    armado=false;                   //Desarma o parquedas e quebra o loop.
-                 }
-            }
-          delay(intervalo);                         //Tempo entre leituras
+    if (alti>apogeu)
+      apogeu=alti;                          //Armazena o apogeu
+    else {
+      if (apogeu-alti>abertura){            //Aciona o pqd em uma distРncia determinada abaixo do apogeu.
+	digitalWrite(PQD_SERVO,HIGH);
+	delay(t_acionamento);           //Aciona o Servo do PQD
+	digitalWrite(PQD_SERVO,LOW);
+	armado=false;                   //Desarma o parquedas e quebra o loop.
+      }
     }
-Serial.print("ABERTURA - Apogeu:");
-Serial.println(apogeu);
+    delay(intervalo);                         //Tempo entre leituras
+  }
+  Serial.print("ABERTURA - Apogeu:");
+  Serial.println(apogeu);
 
-//algoritimo do LED para informar altitude de apogeu
+  //algoritimo do LED para informar altitude de apogeu
   
   mem_apg=apogeu;                                    //Armazena o apogeu em uma variрvel de trabalho, conservando o valor original para referЖncia
-  for(cont=0;cont<3;cont++)                          //Tres piscadas rрpidas sinalizam inьcio de mensagem.
-  { 
-      digitalWrite(LED_1,HIGH);
-      delay(75);
-      digitalWrite(LED_1,LOW);
-      delay(75);
+  for(cont=0;cont<3;cont++) {                         //Tres piscadas rрpidas sinalizam inьcio de mensagem.
+    digitalWrite(LED_1,HIGH);
+    delay(75);
+    digitalWrite(LED_1,LOW);
+    delay(75);
   }
   delay(t_separa);
   
@@ -100,13 +124,12 @@ Serial.println(apogeu);
   
   disp_alt[3]=mem_apg/1000.0;
 
-  for(cont=0;cont<disp_alt[3];cont++)                         
-    {
-      digitalWrite(LED_1,HIGH);
-      delay(t_info);
-      digitalWrite(LED_1,LOW);
-      delay(t_info);
-    }
+  for(cont=0;cont<disp_alt[3];cont++) {
+    digitalWrite(LED_1,HIGH);
+    delay(t_info);
+    digitalWrite(LED_1,LOW);
+    delay(t_info);
+  }
   mem_apg=mem_apg-(disp_alt[3]*1000.0);
   delay(t_separa);  //--------------------------------------
   
@@ -116,13 +139,12 @@ Serial.println(apogeu);
   if(disp_alt[2]==0 && disp_alt[3]>0)
     disp_alt[2]=10;  
                                 
-  for(cont=0;cont<disp_alt[2];cont++)                         
-    {
-      digitalWrite(LED_1,HIGH);
-      delay(t_info);
-      digitalWrite(LED_1,LOW);
-      delay(t_info);
-    }
+  for(cont=0;cont<disp_alt[2];cont++) {
+    digitalWrite(LED_1,HIGH);
+    delay(t_info);
+    digitalWrite(LED_1,LOW);
+    delay(t_info);
+  }
  
   delay(t_separa); //------------------------------------------
   
@@ -132,13 +154,12 @@ Serial.println(apogeu);
   if(disp_alt[1]==0 && disp_alt[2]>0)
     disp_alt[1]=10;  
     
-  for(cont=0;cont<disp_alt[1];cont++)                         
-    {
-      digitalWrite(LED_1,HIGH);
-      delay(t_info);
-      digitalWrite(LED_1,LOW);
-      delay(t_info);
-    }
+  for(cont=0;cont<disp_alt[1];cont++) {
+    digitalWrite(LED_1,HIGH);
+    delay(t_info);
+    digitalWrite(LED_1,LOW);
+    delay(t_info);
+  }
   
   delay(t_separa);  //--------------------------------------
   
@@ -146,40 +167,12 @@ Serial.println(apogeu);
   if(disp_alt[0]==0)
     disp_alt[0]=10;
     
-  for(cont=0;cont<disp_alt[0];cont++)                         
-    {
-      digitalWrite(LED_1,HIGH);
-      delay(t_info);
-      digitalWrite(LED_1,LOW);
-      delay(t_info);
-    }  
+  for(cont=0;cont<disp_alt[0];cont++) {
+    digitalWrite(LED_1,HIGH);
+    delay(t_info);
+    digitalWrite(LED_1,LOW);
+    delay(t_info);
+  }  
   delay(t_separa);  //--------------------------------------
 }
 
-double getPressure()
-{
-  char status;
-  double T,P,p0,a;
-
-  status = altimetro.startTemperature();      //Inicializa o termometro, retorna o tempo de espera em ms ou 0 para erro
-  if (status != 0)
-  {
-    delay(status);                            //Espera atж a finalizar a leitura
-    status = altimetro.getTemperature(T);     //LЖ a temperatura e armazena em T.
-    
-    if (status != 0)
-    {
-      status = altimetro.startPressure(3);    //inicializa o barometro.
-      if (status != 0)
-      {
-        delay(status);
-        status = altimetro.getPressure(P,T);  //LЖ a pressсo e armazena em P, com T como temperatura de referЖncia
-
-        if (status != 0)
-        {
-          return(P);            
-        }
-      }
-    }
-  }
-}
